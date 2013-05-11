b66333fdec793183370383c4714c8a85edf0ec3f
Ben Woodcock <ben@benbillbob.com>
2013-05-11 17:30:25 +1000

    Adding Finish

--- mysite/code/Booking.php
+++ mysite/code/Booking.php
@@ -1,16 +1,9 @@
 <?php
 
-
-
-
-
-
-
-
-
-class Booking extends DataObject {
-	static $db = array(
-		'Time' => 'Text',
-		'Date' => 'Text',
-		'Length' => 'Text');
+class Booking extends DataObject
+{
+    static $db = array(
+        'Time' => 'Text',
+        'Date' => 'Text',
+        'Length' => 'Text');
 }
--- mysite/code/BookingForm.php
+++ mysite/code/BookingForm.php
@@ -2,15 +2,21 @@
 
 class BookingForm extends MultiForm
 {
-	public static $start_step = 'BookingDateForm';
+    public static $start_step = 'BookingDateForm';
 
-	public function finish($data, $form){
-		parent::finish($data, $form);
-		$steps = DataObject::get('MultiFormStep', "SessionID = {$this->session->ID}");
-		if($steps) {
-			foreach($steps as $step) {
-				Debug::show($step->loadData()); // Shows the step data (unserialized by loadData)
-			}
-		}
-	}
+    public function finish($data, $form)
+    {
+        parent::finish($data, $form);
+        $steps = DataObject::get('MultiFormStep', "SessionID = {$this->session->ID}");
+        if ($steps) {
+            foreach ($steps as $step) {
+                //Debug::show($step->loadData());
+            }
+        }
+
+        return array(
+            'Title' => 'Thank you for your submission',
+            'Content' => `You have successfully submitted the form. Thanks!`
+        );
+    }
 }
