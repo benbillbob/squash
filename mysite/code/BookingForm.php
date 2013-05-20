9728b5cba6004a7466ea2eb51afb2eec1c38af09
Ben Woodcock <ben@benbillbob.com>
2013-05-20 22:34:21 +1000

    fixing Finish

--- mysite/code/BookingForm.php
+++ mysite/code/BookingForm.php
@@ -30,9 +30,9 @@ class BookingForm extends MultiForm
         BookingLength:cvb
          */
         $booking = new Booking();
-        $booking->Date->setValue($formData['BookingDate']);
-        $booking->TimeDate->setValue($formData['BookingTime']);
-        $booking->LengthDate->setValue($formData['BookingLength']);
+        $booking->Date = $formData['BookingDate'];
+        $booking->TimeDate = $formData['BookingTime'];
+        $booking->LengthDate = $formData['BookingLength'];
 
         $booking->write();
 
