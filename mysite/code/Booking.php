d56d274c02f6d32877d352e429a59296b3aed610
Ben Woodcock <ben@benbillbob.com>
2013-05-20 22:03:10 +1000

    fixing Finish

--- mysite/code/Booking.php
+++ mysite/code/Booking.php
@@ -3,7 +3,7 @@
 class Booking extends DataObject
 {
     static $db = array(
-        'Time' => 'Text',
-        'Date' => 'Text',
-        'Length' => 'Text');
+        'Date' => 'Date',
+        'Time' => 'Time',
+        'Length' => 'Int');
 }
--- mysite/code/BookingForm.php
+++ mysite/code/BookingForm.php
@@ -7,11 +7,13 @@ class BookingForm extends MultiForm
     public function finish($data, $form)
     {
         parent::finish($data, $form);
-        $steps = DataObject::get('MultiFormStep', "SessionID = {$this->session->ID}");
         $message = "finished<br>";
+
+        $steps = DataObject::get('MultiFormStep', "SessionID = {$this->session->ID}");
+
         if ($steps) {
             foreach ($steps as $step) {
-                $message = $message . '<br>' . implode('|', $step->loadData()) . '<br>';
+                $message = $message . '<br>' . $step->loadData() . '<br>';
             }
         }
 
@@ -21,6 +23,8 @@ class BookingForm extends MultiForm
         $booking->Length = '4532';
 
         $booking->write();
+
+        $this->session->delete();
 
         return $this->controller->customise(array('Form' => false, 'Content' => $message))->renderWith('Page');
     }
